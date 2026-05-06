76fc79d5caede8e8df31f407a53231db35251106
Davide Squeri <squeridavide@gmail.com>
2026-05-06 15:24:54 +0200

    stesura script per testing e creazione grafici

---
+++ keynote.py
@@ -0,0 +1,23 @@
+import time
+from matplotlib import pyplot as plt
+from queens_solver import solve_queens, solve_queens_mod
+
+X = [4,6,8,10,12,14,16,18,20,22,24,26,28,30,32]
+Y = [0.]*15
+for i,x in enumerate(X):
+    time_tot = 0
+    for j in range(4):
+        start_time = time.time()
+        solution = solve_queens(x)
+        elapsed = time.time() - start_time
+        time_tot = time_tot + elapsed
+    #Y[i] = round(time_tot/4, 3)
+    Y[i] = (time_tot/4)*1000
+
+plt.plot(X,Y)
+plt.title("Average time of solution found")
+plt.xlabel("N")
+plt.ylabel("time to complete (ms)")
+plt.grid(True, linestyle='--'
+, alpha=0.7)
+plt.show()
